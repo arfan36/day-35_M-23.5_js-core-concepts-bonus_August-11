4eb71266f2217bf7d6de8437e8995fb7312dd7a4
arfan36 <arfanur36@gmail.com>
2022-08-12 10:34:12 +0600

    23_5-2 Explore Factorial Recursion

---
+++ 1-1_factorial.js
@@ -0,0 +1,27 @@
+// let factorial = 1;
+// for (let i = 5; i >= 1; i--) {
+//     // factorial = factorial * i;
+//     factorial *= i;
+// }
+// console.log(factorial);
+
+function factorial(i) {
+    if (i == 1) {
+        return 1;
+    }
+    return i * factorial(i - 1);
+}
+const result = factorial(5);
+console.log(result);
+/* 
+    i * factorial(i - 1)
+    5 * factorial(5-1)
+    5 * factorial(4)
+    5 * i * factorial(i-1)
+    5 * 4 * factorial(4-1)
+    5 * 4 * factorial(3)
+    5 * 4 * 3 * factorial(2)
+    5 * 4 * 3 * 2 * factorial(1)
+    5 * 4 * 3 * 2 * 1
+    120
+ */
